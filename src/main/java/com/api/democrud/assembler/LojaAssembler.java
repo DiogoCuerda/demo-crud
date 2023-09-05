cd7d60a88cf99f403245a8acd240e4a116755b36
Diogo.Cuerda <diogo352013@gmail.com>
2023-09-05 12:06:03 -0300

    Commit 0509

--- src/main/java/com/api/democrud/assembler/LojaAssembler.java
+++ src/main/java/com/api/democrud/assembler/LojaAssembler.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 public class LojaAssembler {
     public static LojaResponseDTO toResponseModel(Loja loja) {
-        return new LojaResponseDTO(loja.getNome(), loja.getCredito(), setListofProdutoResponse(loja.getProduto()));
+        return new LojaResponseDTO(loja.getId(),loja.getNome(), loja.getCredito(), setListofProdutoResponse(loja.getProduto()));
     }
 
     public static List<LojaResponseDTO> toListResponseModel(List<Loja> loja) {
--- src/main/java/com/api/democrud/dto/response/LojaResponseDTO.java
+++ src/main/java/com/api/democrud/dto/response/LojaResponseDTO.java
@@ -7,12 +7,14 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class LojaResponseDTO {
+    private UUID id;
     private String nome;
     private BigDecimal credito;
     private List<ProdutoResponseDTO> produtos;
